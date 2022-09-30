2d912431428dd90b014281408cb3c349e5f90653
b01010110 <b01010110b@gmail.com>
2022-09-30 16:17:32 +0700

    the index file of the module has been added

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from 'ts-morph'
+export * from './literals'
+
+export * as getLiteralParserByKind from './getLiteralParser'
+
+export * as LiteralKind from './types/LiteralKind'
+export * as KindToTypeMappings from './types/KindToTypeMappings'
